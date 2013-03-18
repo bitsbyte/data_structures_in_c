929c03579268f8b57d2d48a13c22dad35c18f0d0
Himanshu Neema <gigapotentials@gmail.com>
2013-03-19 02:01:06 +0530

    serialize & deserialize binary tree using preorder & postorder traversal. Inorder is not possible as it doesnt give unique traversal

--- bt/serialize_deserialize_bt.h
+++ bt/serialize_deserialize_bt.h
@@ -3,7 +3,20 @@
 #include <string>
 #include <vector>
 
-void serialize(tree *t, std::vector<std::string> &nodes)
+/* INORDER IS NOT UNIQUE: hence generation of a binary tree again is not possible:
+  inorder output is of the form : 
+    $ d1 $ d2 $ d3 $ d4 $ 
+  above  inorder sequence can belong to many trees: 
+        d2                  d3
+      /   \                /  \
+     d1    d4   OR        d2   d4
+          /              /
+         d3             d1
+*/
+
+
+
+void serialize_preorder(tree *t, std::vector<std::string> &nodes)
 {
 	if( t == NULL)
 	{
@@ -12,12 +25,12 @@ void serialize(tree *t, std::vector<std::string> &nodes)
 	}
 
 	nodes.push_back(std::to_string(t->data));
-	serialize(t->left, nodes);
-	serialize(t->right, nodes);
+	serialize_preorder(t->left, nodes);
+	serialize_preorder(t->right, nodes);
 }
 
 
-tree* deserialize(std::vector<std::string> nodes, int &current_index)
+tree* deserialize_preorder(std::vector<std::string> nodes, int &current_index)
 {
 	if( current_index < nodes.size())
 	{
@@ -29,10 +42,45 @@ tree* deserialize(std::vector<std::string> nodes, int &current_index)
 	
 		tree *t = newnode(std::stoi(nodes[current_index]),NULL,NULL);
 		++current_index;
-		t->left = deserialize(nodes, current_index);
-		t->right = deserialize(nodes, current_index);
+		t->left = deserialize_preorder(nodes, current_index);
+		t->right = deserialize_preorder(nodes, current_index);
 		return t;
 	}
 	
+	return NULL;
+}
+
+
+void serialize_postorder(tree *t, std::vector<std::string> &nodes)
+{
+	if( t == NULL)
+	{
+		nodes.push_back("$");
+		return;
+	}
+
+	serialize_postorder(t->left, nodes);
+	serialize_postorder(t->right, nodes);
+	nodes.push_back(std::to_string(t->data));
+}
+
+
+tree* deserialize_postorder(std::vector<std::string> nodes, int &current_index_end)
+{
+	if( current_index_end >= 0)
+	{
+		if(nodes[current_index_end].compare("$") == 0)	
+		{
+			--current_index_end;
+			return NULL;
+		}	
+
+		tree *t = newnode(std::stoi(nodes[current_index_end]), NULL, NULL);	
+		--current_index_end;
+		t->right = deserialize_postorder(nodes, current_index_end);
+		t->left = deserialize_postorder(nodes, current_index_end);
+		
+		return t;
+	}
 	return NULL;
 }
--- bt/test_serialize_deserialize_bt.cpp
+++ bt/test_serialize_deserialize_bt.cpp
@@ -7,27 +7,59 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
+
+void test_sd(tree *t, void (*ser)(tree *t, vector<string> &nodes), 
+										  tree* (*des)(vector<string> nodes, int &cur_idx),
+											int is_postorder, string msg)
 {
-	tree *t = create_random_tree(10,0,20);
-	print_ascii_tree(t);
-	
+	cout << "-----------------------------------------------" << endl;
+	cout << msg << endl;
+	cout << "-----------------------------------------------" << endl;
 	cout << "serializing tree ..." << endl;
 	vector<string> nodes;
-	serialize(t,nodes);
+	ser(t,nodes);
 
 	for(int i = 0 ; i < nodes.size(); ++i)
 		cout << nodes[i] << "\t";
 	cout << endl;
 
 	cout << "deserializing tree ..." << endl;
-	int index =0;
-	tree *dt = deserialize(nodes,index);
-	
+	int index = is_postorder ? (nodes.size() -1) : 0;
+	tree *dt = des(nodes,index);
+
 	print_ascii_tree(dt);
-	
-	free_tree(t);
 	free_tree(dt);	
+}
+
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2)
+	{
+		cout << "usage: test_serialize_deserialize_bt number_of_nodes_in_bt" << endl;
+		return 1;
+	}
+
+	int size = atoi(argv[1]);
+	tree *t = create_random_tree(size,0,20);
+	print_ascii_tree(t);
+	
+	test_sd(t, serialize_preorder, deserialize_preorder, 0, "PREORDER");	
+	
+/* INORDER IS NOT UNIQUE: hence generation of tree again is not possible:
+	inorder output is of the form : 
+		$ d1 $ d2 $ d3 $ d4 $ 
+	above  inorder squence can belong to many trees : 
+				d2                  d3
+			/   \                /  \
+		 d1    d4   OR        d2   d4
+					/              /
+				 d3             d1
+*/
+	
+	test_sd(t, serialize_postorder, deserialize_postorder, 1, "POSTORDER");
+
+	free_tree(t);
 		
 	return 0;
 }
